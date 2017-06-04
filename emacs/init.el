97646f6f8b62e763a44e055bb4b1b5d8306d3abf
dyang <danielyang1009@gmail.com>
2017-06-03 23:15:20 -0400

    update

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;; Daniel Config File
+;; Dyang Config File
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
@@ -7,31 +7,31 @@
 (require 'cl)
 
 ;; Window
+(setq line-spacing 10)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq inhibit-splash-screen t)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-(defvar daniel/packages '(
+(defvar dyang/packages '(
 			  company
-			  monokai-theme
+			  solarized-theme
 			  hungry-delete
 			  swiper
 			  counsel
 			  evil
 			  smartparens
-			  linum-relative
 			  org
 			  ) "Default packages")
-(defun daniel/packages-installed-p ()
-  (loop for pkg in daniel/packages
+(defun dyang/packages-installed-p ()
+  (loop for pkg in dyang/packages
 	when (not (package-installed-p pkg)) do (return nil)
 	finally (return t)))
-(unless (daniel/packages-installed-p)
+(unless (dyang/packages-installed-p)
   (message "%s" "refreshing package database...")
   (package-refresh-contents)
-  (dolist (pkg daniel/packages)
+  (dolist (pkg dyang/packages)
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
@@ -40,14 +40,14 @@
   (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
-(require 'linum-relative)
-(linum-relative-global-mode)
+;(require 'linum-relative)
+;(linum-relative-global-mode)
 
 (global-company-mode t)
 (setq-default cursor-type 'bar)
 (setq make-backup-files nil)
 
-(load-theme 'monokai t)
+(load-theme 'solarized-dark t)
 
 (require 'hungry-delete)         
 (global-hungry-delete-mode)
@@ -68,8 +68,8 @@
 (recentf-mode t)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
-(set-face-attribute 'default nil :font "Source Code Pro-10")
-(set-frame-font "Source Code Pro-10" nil t)
+(set-face-attribute 'default nil :font "Source Code Pro-14")
+(set-frame-font "Source Code Pro-16" nil t)
 
 (require 'smartparens-config)
 ;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
@@ -77,6 +77,8 @@
 
 (require 'org)
 
+(setenv "PATH" (concat "/Library/Tex/texbin" (getenv "PATH")))
+(setq exec-path (append '("/Library/TeX/texbin") exec-path))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -85,7 +87,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default))))
+    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default)))
+ '(package-selected-packages
+   (quote
+    (linum-relative smartparens evil counsel swiper hungry-delete monokai-theme company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
